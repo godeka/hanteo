63357b639aab49424a5fb4f5b8bf1ccecbb8db3e
godeka <godeka@naver.com>
2025-03-20 22:40:58 +0900

    feat: add menus to Appbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+
 import Appbar from "./components/Appbar/Appbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 import "./App.css";
 
+const categories = [
+  { id: 0, title: "차트" },
+  { id: 1, title: "Whook" },
+  { id: 2, title: "이벤트" },
+  { id: 3, title: "뉴스" },
+  { id: 4, title: "스토어" },
+  { id: 5, title: "충전소" },
+];
+
 function App() {
+  const [currCategory, setCurrCategory] = useState(0);
+
   return (
     <div className="App">
-      <Appbar />
+      <Appbar
+        categories={categories}
+        currCategory={currCategory}
+        setCurrCategory={setCurrCategory}
+      />
       <Footer />
     </div>
   );
--- src/components/Appbar/Appbar.jsx
+++ src/components/Appbar/Appbar.jsx
@@ -1,5 +1,28 @@
 import "./Appbar.css";
 
-export default function Appbar() {
-  return <header className="appbar"></header>;
+export default function Appbar({ categories, currCategory, setCurrCategory }) {
+  const handleClickCategory = (id) => {
+    setCurrCategory(id);
+  };
+
+  return (
+    <header className="appbar">
+      <ul>
+        {categories.map((category) => {
+          return (
+            <li key={category.id}>
+              <button
+                className={
+                  currCategory === category.id ? "current-category" : ""
+                }
+                onClick={() => handleClickCategory(category.id)}
+              >
+                {category.title}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </header>
+  );
 }
