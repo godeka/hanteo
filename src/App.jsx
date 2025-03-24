c2308e27b00317f01d0f666f19fc1f5e46ad65b3
godeka <godeka@naver.com>
2025-03-24 14:12:29 +0900

    feat: 모든 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import Footer from "./components/layout/Footer/Footer.jsx";
 import Content from "./components/layout/Content/Content.jsx";
 
 import ChartPage from "./pages/ChartPage.jsx";
+import WhookPage from "./pages/WhookPage.jsx";
+import EventPage from "./pages/EventPage.jsx";
+import NewsPage from "./pages/NewsPage.jsx";
+import StorePage from "./pages/StorePage.jsx";
+import ChargingPage from "./pages/ChargingPage.jsx";
 
 import "./App.css";
-import WhookPage from "./pages/WhookPage.jsx";
 
 const categories = [
   { id: 0, title: "차트", page: <ChartPage /> },
   { id: 1, title: "Whook", page: <WhookPage /> },
-  { id: 2, title: "이벤트" },
-  { id: 3, title: "뉴스" },
-  { id: 4, title: "스토어" },
-  { id: 5, title: "충전소" },
+  { id: 2, title: "이벤트", page: <EventPage /> },
+  { id: 3, title: "뉴스", page: <NewsPage /> },
+  { id: 4, title: "스토어", page: <StorePage /> },
+  { id: 5, title: "충전소", page: <ChargingPage /> },
 ];
 
 function App() {
---
+++ src/pages/StorePage.jsx
@@ -0,0 +1,17 @@
+import Ranking from "../components/ranking/Ranking";
+
+const rankingList = [
+  { id: 0, title: "Unexpected", artist: "THE BOYZ" },
+  { id: 1, title: "Fe304 : FORWARD", artist: "NMIXX" },
+  { id: 2, title: "ZERO : FEVER EPILOGUE", artist: "ATEEZ" },
+  { id: 3, title: "Only One Story", artist: "The Wind" },
+  { id: 4, title: "PLEASURE : 스페셜 미니앨범", artist: "TREASURE" },
+  { id: 5, title: "S", artist: "STAYC" },
+  { id: 6, title: "S", artist: "STAYC" },
+  { id: 7, title: "S", artist: "STAYC" },
+  { id: 8, title: "S", artist: "STAYC" },
+];
+
+export default function StorePage() {
+  return <Ranking title="인기 상품" rankingList={rankingList} />;
+}
